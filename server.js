5eb63c7232ea18c9fdcfec587bd327b91a95405c
Neschetnyi <a.neschetnyi@gmail.com>
2025-04-17 15:35:31 +0500

    5

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use("/api/products", productRoutes);
 
 // Запуск сервера
 const PORT = process.env.PORT || 5000; // Используем переменную PORT из окружения
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err);
+  });
